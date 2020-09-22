450b83563fd16a2fa477c7e35d6cbe2d01db62ec
Yang Shen <cocosuiban@gmail.com>
2020-09-22 21:11:59 +0800

    link

--- demo06/main.js
+++ demo06/main.js
@@ -148,7 +148,7 @@ function demoFromHTML() {
     function (dispose) {
         // dispose: object with X, Y of the last line add to the PDF 
         //          this allow the insertion of new lines after html
-        pdf.save('Test.pdf');
+        pdf.save('Invoice.pdf');
     }, margins);
 }
 
